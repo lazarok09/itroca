a84f87e33c1c067b00d673646a82e580a2004cf3
lazarok09 <lazarok09@gmail.com>
2023-11-07 17:52:03 -0300

    refactor(Login): improve code readability

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,15 @@
-import { LoginPage } from "@/templates/Login";
+
+import { Login } from "@/templates/Login";
 import { Metadata } from "next";
+
 
 // either Static metadata
 export const metadata: Metadata = {
+
   title: "iTroca | Login",
 };
-
-export default function Login() {
-  return <LoginPage />;
+export default function LoginPage() {
+  return (
+      <Login />
+  );
 }
--- src/templates/Login/index.tsx
+++ src/templates/Login/index.tsx
@@ -2,8 +2,14 @@ import { Header } from "@/components/Header";
 import { LoginPageContainer } from "@/containers/LoginPage";
 import { LoginPageWrapper } from "./styles";
 import { Footer } from "@/components/Footer";
+import { Metadata } from "next";
 
-export const LoginPage = () => {
+// either Static metadata
+export const metadata: Metadata = {
+  title: "iTroca | Login",
+};
+
+export const Login = () => {
   return (
     <>
       <Header />
