6403e1f96d94f6160df4c308dd34a58f6b113aaa
lazarok09 <lazarok09@gmail.com>
2023-11-08 18:23:11 -0300

    refactor(HomePresentation): remove component

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -2,7 +2,6 @@ import { Header } from "@/components/Header";
 
 import * as Styled from "./styles";
 
-import { HomePresentation } from "@/components/HomePresentation";
 import { Footer } from "@/components/Footer";
 import { Heading } from "@/components/Heading";
 import { LogoImage } from "@/components/LogoImage";
