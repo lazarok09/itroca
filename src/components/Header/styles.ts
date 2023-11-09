8697abea9d0649d32ff4d04c3f9ff1c19e7d74db
lazarok09 <lazarok09@gmail.com>
2023-11-08 22:14:40 -0300

    refactor(Header): remove not used headerlink

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -23,13 +23,4 @@ export const HeaderNavigation = styled.nav`
   `}
 `;
 
-export const HeaderLink = styled.div<{ active: boolean }>`
-  ${({ theme, active }) => css`
-    background: ${active ? theme.colors.bs_green : "transparent"};
-    padding: 0.6rem 1.4rem;
-    font-weight: 600;
-    border-radius: 8px;
-    box-shadow: 0 0.5rem 1rem #00000026;
-    cursor: pointer;
-  `}
-`;
+
