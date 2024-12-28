94c3f4e5c223f6bded6a8dd7263815e30c16b413
lazarok09 <lazarok09@gmail.com>
2024-12-27 23:41:30 -0300

    feat: add new icons for home products and login

--- src/components/BottonNavigation/index.tsx
+++ src/components/BottonNavigation/index.tsx
@@ -1,16 +1,33 @@
 "use client";
 import { BottomNavigation, BottomNavigationAction, Box } from "@mui/material";
-import RestoreIcon from "@mui/icons-material/Restore";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
+
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import HomeIcon from "@mui/icons-material/Home";
+
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import LoginIcon from "@mui/icons-material/Login";
+
+enum Redirects {
+  products = "products",
+  home = "home",
+  login = "login",
+}
 
 export const FooterBottonNavigation = () => {
   const [value, setValue] = useState(0);
+  const router = useRouter();
+
+  function handleRedirect(destination: keyof typeof Redirects) {
+    if (destination === "home") {
+      return router.push("/");
+    }
+    return router.push(`/${Redirects[destination]}`);
+  }
 
   return (
     <nav className="flex gap-4    items-center justify-center fixed bottom-0 w-full drop-shadow-md lg:drop-shadow-xl">
-      <Box sx={{ width: "100%" }}>
+      <Box sx={{ width: "100%" }} borderColor={"#fffff"}>
         <BottomNavigation
           showLabels
           value={value}
@@ -18,9 +35,21 @@ export const FooterBottonNavigation = () => {
             setValue(newValue);
           }}
         >
-          <BottomNavigationAction label="Dashboard" icon={<RestoreIcon />} />
-          <BottomNavigationAction label="Products" icon={<FavoriteIcon />} />
-          <BottomNavigationAction label="Login" icon={<LocationOnIcon />} />
+          <BottomNavigationAction
+            onClick={() => handleRedirect(Redirects.home)}
+            label="Home"
+            icon={<HomeIcon />}
+          />
+          <BottomNavigationAction
+            label="Produtos"
+            icon={<ShoppingCartIcon />}
+            onClick={() => handleRedirect(Redirects.products)}
+          />
+          <BottomNavigationAction
+            label="Login"
+            icon={<LoginIcon />}
+            onClick={() => handleRedirect(Redirects.login)}
+          />
         </BottomNavigation>
       </Box>
     </nav>
