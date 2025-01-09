313217a11df5a94852f49bcadb68364d7aede4e2
lazarok09 <lazarok09@gmail.com>
2025-01-09 12:36:14 -0300

    feat(Header): add dialog outside click function

--- src/containers/Dialog/index.tsx
+++ src/containers/Dialog/index.tsx
@@ -1,10 +1,36 @@
 import { DialogProps } from "@/context/Dialog";
+import { useEffect } from "react";
 
 type DialogContainerProps = {} & Omit<DialogProps, "setBody">;
 
 export const DialogContainer = ({ dialogRef, body }: DialogContainerProps) => {
+  useEffect(() => {
+    const dialogCloseHandler = (event: MouseEvent) => {
+      const rect =
+        dialogRef?.current && dialogRef?.current.getBoundingClientRect();
+      if (rect) {
+        if (
+          rect.left > event.clientX ||
+          rect.right < event.clientX ||
+          rect.top > event.clientY ||
+          rect.bottom < event.clientY
+        ) {
+          dialogRef.current.close();
+        }
+      }
+    };
+
+    dialogRef?.current?.addEventListener("click", dialogCloseHandler);
+
+    return () => {
+      if (dialogRef?.current) {
+        dialogRef.current.removeEventListener("click", dialogCloseHandler);
+      }
+    };
+  }, [dialogRef]);
+
   return (
-    <dialog className="flex min-h-screen" ref={dialogRef} id="main-dialog">
+    <dialog className="flex min-h-screen " ref={dialogRef} id="main-dialog">
       {dialogRef?.current && dialogRef?.current.open ? body : null}
     </dialog>
   );
