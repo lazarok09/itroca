37cfe6dd1f2ec51685c30cb146b3f76b5708ea18
lazarok09 <lazarok09@gmail.com>
2025-02-04 08:58:16 -0300

    feat(MainHome): add 80vh to main home

--- src/components/MainHome/index.tsx
+++ src/components/MainHome/index.tsx
@@ -12,6 +12,8 @@ export const MainHome = () => (
           items-center
           place-items-center
           gap-1
+          min-h-[80vh]
+
           "
   >
     <div className="motion-scale-in-[0.5] motion-translate-x-in-[-120%] motion-translate-y-in-[-60%] motion-opacity-in-[33%] motion-rotate-in-[-1080deg] motion-blur-in-[10px] motion-delay-[0.38s]/scale motion-duration-[0.38s]/opacity motion-duration-[1.20s]/rotate motion-duration-[0.15s]/blur motion-delay-[0.60s]/blur motion-ease-spring-bouncier ">
